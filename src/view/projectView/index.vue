05072c72544bbfbd6423f2d924fe240de555a0cd
随风 <suifeng.wxb@xuelangyun.com>
2020-07-13 16:54:12 +0800

    组信息展示 canvas初步编写

--- src/view/projectView/index.vue
+++ src/view/projectView/index.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>组内项目任务视图</div>
+  <div class="container">
+    <div class="title">组内项目任务视图</div>
+    <groupCanvas class="groupCanvas"></groupCanvas>
+  </div>
+
 </template>
 
 <script>
+import groupCanvas from '../groupCanvas'
 export default {
+  components:{
+    groupCanvas
+  },
 
+  created() {
+  },
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.container{
+  width: 100%;
+  height: 100%;
+
+  .groupCanvas{
+    width: 100%;
+    height: 90%;
+    border: 1px solid red;
+  }
+}
 
 </style>
